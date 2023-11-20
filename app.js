ec1bf2286b10bef44ce419aed392b59fb1fcd148
Junior98-dev <kounkouddorianjunior@gmail.com>
2023-11-20 15:18:38 +0100

    js btn retour

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const wrapper = document.querySelector(".wrapper");
+const backBtn = document.querySelector(".back-btn");
 const menuBtn = document.querySelector(".menu-btn");
 
 const toggleScreen = () => {
   wrapper.classList.toggle("show-category");
 };
 menuBtn.addEventListener("click", toggleScreen);
-
+backBtn.addEventListener("click", toggleScreen);
