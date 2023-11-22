e88b4d2c464d1b411714c401c83fd8fcbef67740
Junior98-dev <kounkouddorianjunior@gmail.com>
2023-11-22 23:04:38 +0100

    Ajout des categories en JS

--- app.js
+++ app.js
@@ -211,3 +211,50 @@ let tasks = [
     completed: false,
   },
 ];
+
+const categoriesContainer = document.querySelector(".categories");
+
+const renderCategories = () => {
+  categoriesContainer.innerHTML = "";
+  // recuperer toutes les tâches de la catégorie actuelle
+  categories.forEach((category) => {
+    const categoryTasks = tasks.filter(
+      (task) => task.category.toLowerCase() === category.title.toLowerCase()
+    );
+
+    //  create a div to render category
+    const div = document.createElement("div");
+    div.classList.add("category");
+    div.innerHTML = `
+          <div class="left">
+              <img src="images/${category.img}" alt="${category.title}">
+              <div class="content">
+                  <h1>${category.title}</h1>
+                  <p>${categoryTasks.length} Tasks</p>
+              </div>
+          </div>
+          <div class="options">
+              <div class="toggle-btn">
+                  <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
+                  />
+                </svg>
+              </div>
+          </div>
+    `;
+
+    categoriesContainer.appendChild(div);
+  });
+};
+
+renderCategories();
